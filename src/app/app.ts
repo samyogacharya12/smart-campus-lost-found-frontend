2759a96af8499d47267d699bdea56cd8c9fa2a1e
Ayush Thapa <ayush.thapa1417@gmail.com>
2026-05-03 15:40:18 -0500

    Split homepage UI into angular components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Navbar } from './components/navbar/navbar';
+import { Home } from './components/home/home';
+import { HowItWorks } from './components/how-it-works/how-it-works';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Home, HowItWorks],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
