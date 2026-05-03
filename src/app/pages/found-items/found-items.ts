9018fb8abe93d489f4b73822697c53dc12849ed7
Ayush Thapa <ayush.thapa1417@gmail.com>
2026-05-03 16:17:14 -0500

    converted lost and found UI into proper dynamic using angular

--- src/app/pages/found-items/found-items.ts
+++ src/app/pages/found-items/found-items.ts
@@ -4,6 +4,26 @@ import { Component } from '@angular/core';
   selector: 'app-found-items',
   imports: [],
   templateUrl: './found-items.html',
-  styleUrl: './found-items.css',
+  styleUrl: './found-items.css'
 })
-export class FoundItems {}
+export class FoundItems {
+
+  items = [
+    {
+      name: 'Water Bottle',
+      location: 'Student Center',
+      date: 'May 1, 2026'
+    },
+    {
+      name: 'Backpack',
+      location: 'Engineering Building',
+      date: 'May 2, 2026'
+    },
+    {
+      name: 'Keys',
+      location: 'Gym',
+      date: 'May 3, 2026'
+    }
+  ];
+
+}
--- src/app/pages/lost-items/lost-items.ts
+++ src/app/pages/lost-items/lost-items.ts
@@ -4,6 +4,30 @@ import { Component } from '@angular/core';
   selector: 'app-lost-items',
   imports: [],
   templateUrl: './lost-items.html',
-  styleUrl: './lost-items.css',
+  styleUrl: './lost-items.css'
 })
-export class LostItems {}
+export class LostItems {
+  items = [
+    {
+      name: 'Laptop',
+      location: 'Library',
+      date: 'May 1, 2026'
+    },
+    {
+      name: 'Wallet',
+      location: 'Cafeteria',
+      date: 'May 2, 2026'
+    },
+    {
+      name: 'Phone',
+      location: 'Parking Lot',
+      date: 'May 3, 2026'
+    },
+
+    {
+      name: 'AirPods',
+      location: 'Gym',
+      date: 'May 4, 2026'
+    },
+  ];
+}
