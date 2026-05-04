b932c19eeca93bcf7415e6e2725d443bd17bc02a
Deion <davisdeion123@gmail.com>
2026-05-04 15:58:22 -0500

    readme

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  templateUrl: './login.html',
+  styleUrls: ['./login.css']
 })
 export class LoginComponent {
 
